1811851799c19f04cddcdc7e36176913afa47620
Chen JiaJun <90199318+JiaJun8899@users.noreply.github.com>
2023-09-09 22:11:03 +0800

    Project Init 2
    - Included mariadb image for docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME',os.environ.get('DB_NAME')),
+        'USER': os.getenv('DB_USER',os.environ.get('DB_USER')),
+        'PASSWORD': os.getenv('DB_PASSWORD',os.environ.get('DB_PASSWORD')),
+        'HOST': os.getenv('DB_HOST',os.environ.get('DB_HOST')),
+        'PORT': "3306",
     }
 }
 
