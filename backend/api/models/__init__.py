a11861ca66d162c2aed3d2b183ae46538b3644af
tryhardlaijun <wenjun456@gmail.com>
2023-09-17 12:55:35 +0800

    Added NOK

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -3,5 +3,6 @@ from .admin import Admin
 from .normal_user import NormalUser
 from .organiser import Organizer
 from .event import Event
+from .nok import NOK
 from .event_participant import EventParticipant
 
--- backend/api/models/organiser.py
+++ backend/api/models/organiser.py
@@ -5,7 +5,7 @@ from .user import GenericUser
 class Organizer(models.Model):
 
     user = models.OneToOneField( GenericUser, on_delete=models.CASCADE, primary_key=True)
-
+    validOrganisation = models.BooleanField(default=False)
     def __str__(self):
         return str(self.user.first_name)
     class Meta:
