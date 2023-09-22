7db04b84b7c386426bbc121a06cab9435f4fac0d
tryhardlaijun <wenjun456@gmail.com>
2023-09-22 21:37:19 +0800

    Updated Data_faker

--- backend/api/utils/data_faker.py
+++ backend/api/utils/data_faker.py
@@ -28,31 +28,35 @@ def create_generic_users():
     User = get_user_model()
     username = fake.user_name()
     if not User.objects.filter(username=username).exists():
-        user = User.objects.create(username=username, first_name=fake.first_name(), last_name=fake.last_name(),phone=generate_8_digit_phone(), 
+        user = User(username=username, first_name=fake.first_name(), last_name=fake.last_name(),phone=generate_8_digit_phone(), 
                                    nric=generate_singapore_nric(), email=username+"@gmail.com",)
         user.set_password("test_password")
+        user.save()
         return user
     return None
 
 def create_admins():
     user = create_generic_users()
-    Admin.objects.create(user=user)
+    if(user == None):
+        print("USER IS NONE")
+        return
+    Admin.adminManager.create(user=user)
 
 
 def create_normal_users(num):
     for _ in range(num):
         user = create_generic_users()
-        NormalUser.objects.create(user=user,birthday=fake.date())
+        NormalUser.normalUserManager.create(user=user,birthday=fake.date())
 
 
 def create_organizers(num):
     for _ in range(num):
         user = create_generic_users()
-        Organizer.objects.create(user=user,validOrganisation =True)
+        Organizer.organiserManager.create(user=user,validOrganisation =True)
 
 def create_events(num):
     for _ in range(num):
-        Event.objects.create(
+        Event.eventManager.create(
             eventName=fake.company(),
             startDate=timezone.now(),
             endDate=timezone.now() + timezone.timedelta(days=2),
@@ -71,7 +75,7 @@ def create_NOKs(num):
 
 
 def create_emergency_contacts():
-    all_normal_users = NormalUser.objects.all()
+    all_normal_users = NormalUser.normalUserManager.all()
     all_nok = NOK.objects.all()
 
     nok_count = len(all_nok)
@@ -89,8 +93,8 @@ def create_emergency_contacts():
 
 def create_event_organizer_mappings():
     try:
-        for event in Event.objects.all():
-            for organizer in Organizer.objects.all():
+        for event in Event.eventManager.all():
+            for organizer in Organizer.organiserManager.all():
                 # Check for existing mapping
                 existing_mapping = EventOrganizerMapping.objects.filter(event=event, organizer=organizer).exists()
 
@@ -101,8 +105,8 @@ def create_event_organizer_mappings():
         print(f"An error occurred: {e}")
 
 def create_event_participants():
-    for event in Event.objects.all():
-        for normal_user in NormalUser.objects.all():
+    for event in Event.eventManager.all():
+        for normal_user in NormalUser.normalUserManager.all():
             EventParticipant.objects.create(event=event, participant=normal_user)
 
 @transaction.atomic
@@ -114,7 +118,7 @@ def runfile():
         create_admins()
         create_normal_users(100)
         create_organizers(100)
-        create_events(10)
+        create_events(100)
         create_NOKs(30)
         create_emergency_contacts()
         create_event_organizer_mappings()
