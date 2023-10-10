d3aabe5d06364afce25bdb8cbe6379852c28cda6
tryhardlaijun <wenjun456@gmail.com>
2023-10-10 13:50:43 +0800

    Added missing serializer and urls file

---
+++ backend/api/serializer/serializer.py
@@ -0,0 +1,49 @@
+# serializers.py
+
+from api.models import *
+from rest_framework import serializers
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Admin
+        fields = '__all__'
+
+class EmergencyContactsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmergencyContacts
+        fields = '__all__'
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class EventOrganizerMappingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventOrganizerMapping
+        fields = '__all__'
+
+class EventParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventParticipant
+        fields = '__all__'
+
+class GenericUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GenericUser
+        fields = '__all__'
+
+class NOKSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NOK
+        fields = '__all__'
+
+class NormalUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NormalUser
+        fields = '__all__'
+
+class OrganizerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organizer
+        fields = '__all__'
---
+++ backend/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('update-organizer-status/', views.UpdateOrganizerStatus.as_view(), name='update_organizer_status'),
+    path('get-all-organizers/', views.GetAllOrganizers.as_view(), name='get_all_organizers'),
+]
+
