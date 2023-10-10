13f33735b254077393504797c952c3219b6b86c2
tryhardlaijun <wenjun456@gmail.com>
2023-10-10 16:33:38 +0800

    Added Admin feature

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from api.models import Admin, Organizer,NormalUser,GenericUser
 # Register your models here.
+
+admin.site.register(Admin)
+admin.site.register(Organizer)
+admin.site.register(NormalUser)
+admin.site.register(GenericUser)
