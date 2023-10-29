2c4a593af6f5306e67d91e6b40dba3f34a6ce81f
tryhardlaijun <wenjun456@gmail.com>
2023-10-29 15:46:54 +0800

    Updated admin.py feature less deleting

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,38 +1,64 @@
+from django.contrib.admin.models import LogEntry
 from django.contrib import admin
 from api.models import Admin, Organizer,NormalUser,GenericUser,Event,EventOrganizerMapping,EventParticipant
 from django.contrib.auth.models import Group
 # Register your models here.
 from django_otp.admin import OTPAdminSite
 admin.site.__class__ = OTPAdminSite
+from django.contrib import admin
+from api.models import Admin, Organizer, NormalUser, GenericUser, Event, EventOrganizerMapping, EventParticipant
+from django.contrib.auth.models import Group
+from django_otp.admin import OTPAdminSite
 
-@admin.register(Organizer)
-class OrganizerAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
+admin.site.__class__ = OTPAdminSite
+
+class BaseAdmin(admin.ModelAdmin):
+    # Deny add permission by default
+    def has_add_permission(self, request, obj=None):
         return False
 
-
-@admin.register(GenericUser)
-class GenericUserAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        return False
-
+    # Deny change permission by default
     def has_change_permission(self, request, obj=None):
         return False
 
+    # Deny delete permission by default
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(Organizer)
+
+class OrganizerAdmin(BaseAdmin):
+    # Deny delete permission by default
+    readonly_fields = ["user"]
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+@admin.register(NormalUser)
+class GenericUserAdmin(BaseAdmin):
     exclude = ["password"]
 
 @admin.register(Admin)
-class AdminAdmin(admin.ModelAdmin):
+class AdminAdmin(BaseAdmin):
     pass
 
 @admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
+class EventAdmin(BaseAdmin):
     pass
 
 @admin.register(EventParticipant)
-class EventParticipantAdmin(admin.ModelAdmin):
+class EventParticipantAdmin(BaseAdmin):
     pass
+
 @admin.register(EventOrganizerMapping)
-class EventOrganizerMappingAdmin(admin.ModelAdmin):
-    pass
+class EventOrganizerMappingAdmin(BaseAdmin):
+    readonly_fields = ["event", "organizer"]
+
+    def has_change_permission(self, request, obj=None):
+        return True
+""" admin.site.unregister """
 admin.site.unregister(Group)
+admin.site.register(LogEntry)
