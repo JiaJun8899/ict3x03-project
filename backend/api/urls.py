1f82b158a3fab6c7be89ecd600a2f4a6ba7df32d
tryhardlaijun <wenjun456@gmail.com>
2023-10-30 22:59:51 +0800

    Added name for reverse() in test case and completed login test case

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    # Organisation
+urlpatterns =[
     path('get-all-events/', views.EventAPI.as_view(), name='get-all-events'),
     path('get-event-byorg/', views.EventsByOrganizationAPI.as_view(), name='get-event-org'),
     path('get-single-event/<str:event_id>', views.EventSingleByOrganizationAPI.as_view(), name='get-single-event'),
@@ -15,15 +14,15 @@ urlpatterns = [
     path('profile/',views.GetProfileDetailsAPIView.as_view(), name="get-profile"),       
     path('search-events/', views.SearchEvents.as_view(), name="serach-events"),
     path('get-all-events/', views.GetAllEvent.as_view(), name='get_all_events'),
-    path('get-event/<str:eid>',views.GetEvent.as_view()),
-    path('auth-login/', views.Login.as_view(), name='get_all_events'),
     path('csrf/', views.csrf),
     path('get-all-events/', views.GetAllEvent.as_view(), name='get_all_events'),
-    path('auth-verify-OTP/', views.VerifyOtp.as_view(), name='get_all_events'),
-    path('auth-get-OTP/', views.GetOTP.as_view(), name='get_all_events'),
-    path('auth-logout/', views.Logout.as_view(), name='get_all_events'),
-    path('auth-reset-password/', views.ResetPassword.as_view()),
-    path('auth-change-password/',views.ChangePassword.as_view()),
     path('check-auth', views.TestAPI.as_view()),
-    path('check-valid-organizer/<str:eid>',views.validEventOrg)
+    path('check-valid-organizer/<str:eid>',views.validEventOrg),
+    path('auth-login/', views.Login.as_view(), name='auth-login'),
+    path('auth-verify-OTP/', views.VerifyOtp.as_view(), name='auth-verify-OTP'),
+    path('auth-get-OTP/', views.GetOTP.as_view(), name='auth-get-OTP'),
+    path('auth-logout/', views.Logout.as_view(), name='auth-logout'),
+    path('auth-reset-password/', views.ResetPassword.as_view(), name='auth-reset-password'),
+    path('auth-change-password/',views.ChangePassword.as_view(), name='auth-change-password'),
+    path('csrf/', views.csrf),
 ]
