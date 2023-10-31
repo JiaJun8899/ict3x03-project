2460afd72ff3772bce99a8d819c2337b378173c1
Chen JiaJun <90199318+JiaJun8899@users.noreply.github.com>
2023-10-31 14:46:34 +0800

    Added more test cases

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -16,15 +16,36 @@ load_dotenv()
 
 class GetAllEventTest(APITestCase):
     def setUp(self):
+        self.client = APIClient()
         Event.eventManager.create(eventName="test Event", startDate="2023-10-13T04:34:00+08:00", endDate = "2023-10-26T04:34:00+08:00", eventImage = None, eventStatus= "open", noVol= 1233, eventDesc=123123)
         Event.eventManager.create(eventName="test Event1", startDate="2023-10-13T04:34:00+08:00", endDate = "2023-10-26T04:34:00+08:00", eventImage = None, eventStatus= "open", noVol= 1233, eventDesc=123123)
         Event.eventManager.create(eventName="test Event2", startDate="2023-10-13T04:34:00+08:00", endDate = "2023-10-26T04:34:00+08:00", eventImage = None, eventStatus= "open", noVol= 1233, eventDesc=123123)
 
-    def test_all_events(self):
+    def test_all_events_Fail(self):
+        url = reverse('get-all-events')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_all_event(self):
+        norm_generic = GenericUser.objects.create_user(
+            username='test@test.com',
+            email='test@test.com',
+            first_name='test',
+            last_name='test',
+            phoneNum='91234567',
+            nric='S1234567H',
+            password='testiepassword',
+        )
+        self.test_org = NormalUser.normalUserManager.create(user_id=norm_generic.id, birthday="2001-03-12")
+        self.client.login(username="test@test.com", password="testiepassword")
+        session = self.client.session
+        session['role'] = "normal"
+        session.save()
         url = reverse('get-all-events')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
+
 
 class RegisterTest(APITestCase):
     def setUp(self):
@@ -51,32 +72,44 @@ class RegisterTest(APITestCase):
         NormalUser.normalUserManager.create(user_id=norm_generic.id, birthday="2001-03-12")
     
     def test_create_Organizer(self):
-        data = {"email": "jiajun@org.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "s9123456h", "password": "jiajun123", "password2": "jiajun123","organization": True, "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
+        data = {"email": "jiajun@org.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "456h", "password": "jiajun123", "password2": "jiajun123","organization": True, "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
         url = reverse('register')
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(Organizer.organizerManager.getAllRecords()), 2)
 
     def test_create_Normal(self):
-        data = {"email": "jiajun@normal.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "s9123456h", "password": "jiajun123", "password2": "jiajun123","organization": False, "birthday": "2007-10-17", "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
+        data = {"email": "jiajun@normal.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "456h", "password": "jiajun123", "password2": "jiajun123","organization": False, "birthday": "2007-10-17", "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
         url = reverse('register')
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(NormalUser.normalUserManager.getAllRecords()), 2)
     
-    def test_clash_create_Organizer(self):
-        data = {"email": "test@org.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "s9123456h", "password": "jiajun123", "password2": "jiajun123","organization": True, "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
+    def test_create_Organizer_fail_inputs(self):
+        data = {"email": "test@org.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":912334567, "NRIC": "4456h", "password": "password", "password2": "password","organization": True, "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
+        url = reverse('register')
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data), 5)
+    
+    def test_Organizer_fail_special_char(self):
+        data = {"email": "jiajun123@org.com", "firstName": "<jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "456h", "password": "jiajun123", "password2": "jiajun123","organization": True, "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
         url = reverse('register')
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_clash_create_Normal(self):
-        data = {"email": "test@normal.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "s9123456h", "password": "jiajun123", "password2": "jiajun123","organization": False, "birthday": "2007-10-17", "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
+        data = {"email": "test@normal.com", "firstName": "jiajun", "lastName": "jiajun", "phoneNum":91234567, "NRIC": "456h", "password": "jiajun123", "password2": "jiajun123","organization": False, "birthday": "2007-10-17", "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
         url = reverse('register')
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
-
+    
+    def test_Normal_fail_special_char(self):
+        data = {"email": "jiajun@normal.com", "firstName": "jiajun", "lastName": "<jiajun", "phoneNum":91234567, "NRIC": "456h", "password": "jiajun123", "password2": "jiajun123","organization": False, "birthday": "2007-10-17", "recaptchaValue": "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI", "SECRET_KEY":os.getenv("TEST_INPUT", os.environ.get("TEST_INPUT"))}
+        url = reverse('register')
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
 class OrganizerTest(APITestCase):
     def generate_photo_file(self):
         file = io.BytesIO()
@@ -98,8 +131,8 @@ class OrganizerTest(APITestCase):
             password='testiepassword',
         )
         self.test_org = Organizer.organizerManager.create(user_id=self.test_user.id)
-        event = Event.eventManager.create(eventName="test Event", startDate="2023-10-13T04:34:00+08:00", endDate = "2023-10-26T04:34:00+08:00", eventImage = None, eventStatus= "open", noVol= 1233, eventDesc=123123)
-        self.eventMap = EventOrganizerMapping.eventMapperManager.create(event_id = event.eid, organizer_id = self.test_org.user_id)
+        self.event = Event.eventManager.create(eventName="test Event", startDate="2023-10-13T04:34:00+08:00", endDate = "2023-10-26T04:34:00+08:00", eventImage = None, eventStatus= "open", noVol= 1233, eventDesc=123123)
+        self.eventMap = EventOrganizerMapping.eventMapperManager.create(event_id = self.event.eid, organizer_id = self.test_org.user_id)
         self.client.login(username="test@test.com", password="testiepassword")
         session = self.client.session
         session['role'] = "organizer"
@@ -115,16 +148,33 @@ class OrganizerTest(APITestCase):
         response = self.client.get(url)
         self.assertEqual(len(response.data), 2)
     
+    def test_create_event_fail_dates(self):
+        photo_file = self.generate_photo_file()
+        data = {"eventName": "Test Create", "startDate": timezone.now(),"endDate": timezone.now() - datetime.timedelta(days=2),"noVol": 12,"eventDesc": "This is for test","eventImage": photo_file}
+        url = reverse('get-event-org')
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
     def test_edit_event(self):
         photo_file = self.generate_photo_file()
         url = reverse('get-event-org')
         response = self.client.get(url)
         eid = response.data[0]['event']['eid']
-        data = {"eid": eid, "eventName": "Test Updated","eventImage": photo_file}
+        data = {"eid": eid, "eventName": "<Test Updated","eventImage": photo_file}
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response = self.client.get(url)
-        self.assertEqual(response.data[0]['event']['eventName'], "Test Updated")
+        self.assertEqual(response.data[0]['event']['eventName'], "&lt;Test Updated")
+    
+    def test_edit_event(self):
+        url = reverse('get-event-org')
+        response = self.client.get(url)
+        eid = response.data[0]['event']['eid']
+        data = {"eid": eid, "endDate": "2023-10-10T04:34:00+08:00"}
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response = self.client.get(url)
+        self.assertEqual(response.data[0]['event']['eventName'], "test Event")
     
     def test_delete_event(self):
         url = reverse('get-event-org')
@@ -144,6 +194,52 @@ class OrganizerTest(APITestCase):
         for image_file in image_files:
             if os.path.exists(image_file):
                 os.remove(image_file)
+
+
+class OrganizerTestInvalid(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.test_user = GenericUser.objects.create_user(
+            username='test@test.com',
+            email='test@test.com',
+            first_name='test',
+            last_name='test',
+            phoneNum='91234567',
+            nric='567H',
+            password='testiepassword',
+        )
+        self.test_org = Organizer.organizerManager.create(user_id=self.test_user.id)
+        self.event = Event.eventManager.create(eventName="test Event", startDate="2023-10-13T04:34:00+08:00", endDate = "2023-10-26T04:34:00+08:00", eventImage = None, eventStatus= "open", noVol= 1233, eventDesc=123123)
+        self.eventMap = EventOrganizerMapping.eventMapperManager.create(event_id = self.event.eid, organizer_id = self.test_org.user_id)
+        
+        self.test_user_2 = GenericUser.objects.create_user(
+            username='test12@test12.com',
+            email='test12@test12.com',
+            first_name='test',
+            last_name='test',
+            phoneNum='91234567',
+            nric='567H',
+            password='testiepassword',
+        )
+        self.test_org_2 = Organizer.organizerManager.create(user_id=self.test_user_2.id)
+        self.client.login(username="test12@test12.com", password="testiepassword")
+        session = self.client.session
+        session['role'] = "organizer"
+        session.save()
+    
+    def test_edit_event_fail(self):
+        url = reverse('get-event-org')
+        response = self.client.get(url)
+        data = {"eid": self.event.eid, "eventName": "Test Updated"}
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_delete_event(self):
+        url = reverse('get-event-org')
+        response = self.client.get(url)
+        data = {"eid": self.event.eid}
+        response = self.client.delete(url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class LoginAPITests(APITestCase):
